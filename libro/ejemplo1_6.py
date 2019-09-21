d1fbf857a6ac5a0670c994a8d6246b6eb93ddac4
Zamudio2024 <fabianzamudio38@aragon.unam.mx>
2019-09-21 18:20:55 -0500

    ejercicios
    git commit -m ejercicios
    
    git status
    git add
    git commit

---
+++ libro/ejemplo1_6.py
@@ -0,0 +1,9 @@
+A = int(input("primer numero:"))
+B = int(input("segundo numero:"))
+C = int(input("tercer numero:"))
+D = int(input("cuarto numero:"))
+print(D)
+print(C)
+print(B)
+print(A)
+
---
+++ libro/ejemplo1_8.py
@@ -0,0 +1,9 @@
+MAT = int(input("Matricula del alumno:"))
+CAL1 = float(input("calificaion 1 del alumno:"))
+CAL2 = float(input("calificaion 2 del alumno:"))
+CAL3 = float(input("calificaion 3 del alumno:"))
+CAL4 = float(input("calificaion 4 del alumno:"))
+CAL5 = float(input("calificaion 5 del alumno:"))
+PRO = (CAL1 + CAL2 + CAL3 + CAL4 + CAL5) / 5
+print(f"Matricula del alumno: {MAT}")
+print(f"Promedio del alumno: {PRO}")
