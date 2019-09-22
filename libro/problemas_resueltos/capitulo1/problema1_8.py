27df92262fa43922350f782e1f0f66aaa2706777
Zamudio2024 <fabianzamudio38@aragon.unam.mx>
2019-09-21 19:01:02 -0500

    Tarea

---
+++ libro/problemas_resueltos/capitulo1/problema1_8.py
@@ -0,0 +1,6 @@
+X1 = float(input("Introduzca el valor de X1 de P1:"))
+Y1 = float(input("Introduzca el valor de Y1 de P1:"))
+X2 = float(input("Introduzca el valor de X2 de P2:"))
+Y2 = float(input("Introduzca el valor de Y2 de P2:"))
+DIS = ((X1 - X2) ** 2 + (Y1 - Y2) ** 2) ** 0.5
+print(f"El valor de la distancia entre P1 y P2 es: {DIS}")
