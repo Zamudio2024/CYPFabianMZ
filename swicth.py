7c74d097654e7e60b19bc2c31420cf7254341345
Zamudio2024 <fabianzamudio38@aragon.unam.mx>
2019-10-11 23:29:58 -0500

    swicth

--- swicth.py
+++ swicth.py
@@ -1,12 +1,15 @@
-dia = int(input("Itroduce un dia (1-5)
-if dia == 1:
-	print("Lunes")
+dia = int(input("Introduce un dia(1-5)"))
+if dia == 1 :
+    print("Lunes")
 elif dia == 2:
-	print("
-eliff dia == 2:
-        print("Martres")
-~lif dia == 2:
-        print("Martres")
-~elif dia == 2:
-        print("Martres")
-~Martres")
+    print("Martes")
+elif dia == 3:
+    print("Miércoles")
+elif dia == 4:
+    print("Jueves")
+elif dia == 5:
+    print("Viernes")
+else:
+    print("Opción no válida")
+
+print("Fin del programa!!")
