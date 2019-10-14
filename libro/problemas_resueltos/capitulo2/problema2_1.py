5b597ec7d2d00e97ec6f053a85d479dfa71eb306
Zamudio2024 <fabianzamudio38@aragon.unam.mx>
2019-10-14 17:29:19 -0500

    tarea

---
+++ libro/problemas_resueltos/capitulo2/problema2_1.py
@@ -0,0 +1,5 @@
+N = int(input("Numero de sonidos que emitio el grillo:"))
+if N > 0:
+	T = (N/4) + 40
+	print(f"La temperatura es de {T} grados Fahrenheit")
+print("Fin del programa")
---
+++ libro/problemas_resueltos/capitulo2/problema2_2.py
@@ -0,0 +1,5 @@
+P = int(input("Introduzca un numero entero:"))
+Q = int(input("Introduzca otro numero entero:"))
+EXP = P**3 + Q**4-2*P**2
+if EXP < 680:
+	print(f"El resultado de P es {EXPprint(f"El resultado de Q es 
