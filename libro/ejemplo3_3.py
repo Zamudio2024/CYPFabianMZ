f85c282041d9d55673e642ce826f97cd1163bb51
Zamudio2024 <fabianzamudio38@aragon.unam.mx>
2019-10-15 17:27:29 -0500

    Ejercicios for y while

---
+++ libro/ejemplo3_3.py
@@ -0,0 +1,7 @@
+CUECER =0
+N = int(input("Dame unh numero entero positivo:"))
+for i in range(0, N, 1):
+    NUM = int(input("Ingresa un numero entero:"))
+    if NUM ==0:
+        CUECER +=1
+print("El numero de 0's capturados fue:", CUECER)
