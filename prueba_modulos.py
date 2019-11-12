f860494b8610d3c29c9a61d014676a5963c3d073
Zamudio2024 <fabianzamudio38@aragon.unam.mx>
2019-11-12 17:55:39 -0600

    Ejercicios 12/11/19

---
+++ prueba_modulos.py
@@ -0,0 +1,20 @@
+#import modulos
+#modulos.mi_print("Hola")
+
+from modulos import *
+import time
+import sys
+from asciistuff import Banner
+
+mi_print("Hola de nuevo")
+otro_print("Otro print usado")
+print(sumar(4,5))
+print(restar(10,7))
+
+for i in range(10,0,-1):
+    print(i,"...")
+    time.sleep(.25)
+print(Banner("BOOOOOM!!!!!!"))
+
+print(sys.platform)
+
